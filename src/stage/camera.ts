6e8922a6956e17606429987682f3fc5011886408
Crystal Jin <kstjxy@gmail.com>
2025-10-08 14:56:29 -0400

    Finish naive

--- src/stage/camera.ts
+++ src/stage/camera.ts
@@ -7,7 +7,8 @@ class CameraUniforms {
     private readonly floatView = new Float32Array(this.buffer);
 
     set viewProjMat(mat: Float32Array) {
-        // TODO-1.1: set the first 16 elements of `this.floatView` to the input `mat`
+        // copy the 4x4 matrix (16 floats) into the backing buffer
+        this.floatView.set(mat.subarray(0, 16), 0);
     }
 
     // TODO-2: add extra functions to set values needed for light clustering here
@@ -33,10 +34,12 @@ export class Camera {
     keys: { [key: string]: boolean } = {};
 
     constructor () {
-        // TODO-1.1: set `this.uniformsBuffer` to a new buffer of size `this.uniforms.buffer.byteLength`
-        // ensure the usage is set to `GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST` since we will be copying to this buffer
-        // check `lights.ts` for examples of using `device.createBuffer()`
-        //
+        // create GPU buffer for camera uniforms (view-projection matrix)
+        this.uniformsBuffer = device.createBuffer({
+            label: "camera uniforms",
+            size: this.uniforms.buffer.byteLength,
+            usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+        });
         // note that you can add more variables (e.g. inverse proj matrix) to this buffer in later parts of the assignment
 
         this.projMat = mat4.perspective(toRadians(fovYDegrees), aspectRatio, Camera.nearPlane, Camera.farPlane);
@@ -128,11 +131,12 @@ export class Camera {
         const lookPos = vec3.add(this.cameraPos, vec3.scale(this.cameraFront, 1));
         const viewMat = mat4.lookAt(this.cameraPos, lookPos, [0, 1, 0]);
         const viewProjMat = mat4.mul(this.projMat, viewMat);
-        // TODO-1.1: set `this.uniforms.viewProjMat` to the newly calculated view proj mat
+        // upload latest view-projection matrix to host-side uniform buffer
+        this.uniforms.viewProjMat = viewProjMat as unknown as Float32Array;
 
         // TODO-2: write to extra buffers needed for light clustering here
 
-        // TODO-1.1: upload `this.uniforms.buffer` (host side) to `this.uniformsBuffer` (device side)
-        // check `lights.ts` for examples of using `device.queue.writeBuffer()`
+        // upload host-side buffer to device uniform buffer
+        device.queue.writeBuffer(this.uniformsBuffer, 0, this.uniforms.buffer);
     }
 }
